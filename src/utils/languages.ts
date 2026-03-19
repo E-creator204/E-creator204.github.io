d578f7cda466a3a86bb50c3999d96a9452f27ff1
E-creator204 <dante.zsh204@gmail.com>
2026-03-18 20:28:20 -0500

    update

--- src/utils/languages.ts
+++ src/utils/languages.ts
@@ -2,28 +2,34 @@ export interface Language {
   name: string;
   iconName: string;
   className?: string;
+  darkIcon?: boolean;
 }
 
 export const languages: Record<string, Language> = {
   linux: {
     name: "Linux",
     iconName: "linux",
+    darkIcon: true,
   },
   wireshark: {
     name: "Wireshark",
     iconName: "wireshark",
+    darkIcon: true,
   },
   metasploit: {
     name: "Metasploit",
     iconName: "metasploit",
+    darkIcon: true,
   },
   owasp: {
     name: "OWASP",
     iconName: "owasp",
+    darkIcon: true,
   },
   zap: {
     name: "OWASP ZAP",
     iconName: "zap",
+    darkIcon: true,
   },
   grafana: {
     name: "Grafana",
